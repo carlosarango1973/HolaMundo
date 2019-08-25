5862271ee80365bb773fb3c2ac86d602cd38d211
Carlos Arango Betancurt <carlos.arango.b.2019@gmail.com>
2019-08-25 11:43:03 -0500

    creacion de nuevo  proyecto en c#

---
+++ HolaMundo2/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace HolaMundo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.btnpresioname = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Wide Latin", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "escribe tu nombre";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(345, 82);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(141, 20);
+            this.txtnombre.TabIndex = 1;
+            // 
+            // btnpresioname
+            // 
+            this.btnpresioname.Location = new System.Drawing.Point(519, 84);
+            this.btnpresioname.Name = "btnpresioname";
+            this.btnpresioname.Size = new System.Drawing.Size(75, 23);
+            this.btnpresioname.TabIndex = 3;
+            this.btnpresioname.Text = "presioname";
+            this.btnpresioname.UseVisualStyleBackColor = true;
+            this.btnpresioname.Click += new System.EventHandler(this.btnpresioname_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(334, 36);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "BIENVENIDO AL PROGRAMA";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnpresioname);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Button btnpresioname;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
